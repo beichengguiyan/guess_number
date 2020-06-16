a82be8adff4732967f760a262f4929ccb4e3f64d
beichengguiyan <chihaotian@zybank.com.cn>
2020-06-16 20:32:17 +0800

    should_return_0A0B_when_given_1234_5678

--- src/test/java/com/twschool/practice/GuessNumberTest.java
+++ src/test/java/com/twschool/practice/GuessNumberTest.java
@@ -13,10 +13,41 @@ public class GuessNumberTest {
         String answer = "1234";
         String input ="1234";
         String result =null;
+
         // when
         result = guessNumber.judgeNumberAndPosition(answer,input);
+
         // then
-//        assert result.equals("4A0B");
         Assert.assertThat(result, is("4A0B"));
+    }
+
+    @Test
+    public void should_return_0A0B_when_given_1234_5678() {
+        // given
+        GuessNumber guessNumber = new GuessNumber();
+        String answer = "1234";
+        String input ="5678";
+        String result =null;
+
+        // when
+        result = guessNumber.judgeNumberAndPosition(answer,input);
+
+        // then
+        Assert.assertThat(result, is("0A0B"));
+    }
+
+    @Test
+    public void should_return_1A2B_when_given_1234_2436() {
+        // given
+        GuessNumber guessNumber = new GuessNumber();
+        String answer = "1234";
+        String input ="2436";
+        String result =null;
+
+        // when
+        result = guessNumber.judgeNumberAndPosition(answer,input);
+
+        // then
+        Assert.assertThat(result, is("1A2B"));
     }
 }
